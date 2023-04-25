5339e5b93b99d2ab8c02de75e520f6c51c3e5285
nicolas-ruth1 <c21388736@mytudublin.ie>
2023-04-25 18:02:18 +0100

    update3

--- MusicVisuals-master/java/src/c21716601/nico.java
+++ MusicVisuals-master/java/src/c21716601/nico.java
@@ -12,34 +12,34 @@ public class nico {
 
     public void render() {
         nico.noFill();
-        nico.camera(0, 100, 200, 0, 0, 0, 1, 0, 0);
-        nico.translate(0, 0, 0);
+        nico.camera(-200, -200, 200, 0, 0, 0, 0, 0, 1);
+        nico.translate(-100, -100, 0);
         nico.strokeWeight(4);
 
         float[] b = nico.getSmoothedBands();
         nico.stroke(150, 250, 130);
         float size = b[2];
         nico.pushMatrix();
-        nico.translate(-size / 2, -size / 2, size / 2);
+        nico.translate(0, 0, size / 2);
         nico.box(size);
         nico.popMatrix();
 
         nico.noFill();
-        nico.camera(0, -100, 200, 0, 0, 0, 5, 0, 0);
-        nico.translate(0, 0, 0);
+        nico.camera(0, 0, 200, 0, 0, 0, 0, -1, 0);
+        nico.translate(-100, -100, 0);
         nico.strokeWeight(4);
 
         float[] b1 = nico.getSmoothedBands();
         nico.stroke(150, 250, 130);
         float size1 = b1[2];
         nico.pushMatrix();
-        nico.translate(-size1 / 2, -size1 / 2, size1 / 2);
+        nico.translate(0, 0, size1 / 2);
         nico.box(size1);
         nico.popMatrix();
 
         nico.noFill();
-        nico.camera(0, 0, 200, 0, 0, 0, 5, 0, 0);
-        nico.translate(0, 0, 0);
+        nico.camera(200, -200, 200, 0, 0, 0, 0, 0, 1);
+        nico.translate(-100, -100, 0);
         nico.strokeWeight(2);
 
         float[] b2 = nico.getSmoothedBands();
@@ -48,7 +48,7 @@ public class nico {
             nico.stroke(c, 0, 102);
             float s = b2[i];
             nico.pushMatrix();
-            nico.translate(-s / 2, -s / 2, s / 2);
+            nico.translate(0, 0, s / 2);
             nico.box(s);
             nico.popMatrix();
         }
