ffe34e9fb632e37c36e7b364d82d1e282e93eee9
nicolas-ruth1 <c21388736@mytudublin.ie>
2023-04-25 18:45:20 +0100

    update9

--- MusicVisuals-master/java/src/c21716601/nico.java
+++ MusicVisuals-master/java/src/c21716601/nico.java
@@ -11,6 +11,8 @@ public class nico {
     }
 
     public float rotationAngle = 0.0f;
+    public float circleRadius = 100.0f;
+    public int numDots = 12;
 
     public void render() {
         nico.noFill();
@@ -19,45 +21,29 @@ public class nico {
         nico.strokeWeight(4);
 
         float[] b = nico.getSmoothedBands();
-        nico.stroke(150, 250, 130);
+        nico.stroke(0, 255, 0); // Green stroke color
         float size = b[2];
 
-        // Apply rotation transformation
+        // Apply rotation transformation to the squares
         nico.pushMatrix();
         nico.rotateZ(rotationAngle);
         nico.rect(-size / 2, -size / 2, size, size);
         nico.popMatrix();
 
+        // Render spinning dots around the squares
         nico.noFill();
-        nico.camera(0, -100, 200, 0, 0, 0, 5, 0, 0);
-        nico.translate(0, 0, 0);
-        nico.strokeWeight(4);
+        nico.stroke(255); // White stroke color for dots
 
-        float[] b1 = nico.getSmoothedBands();
-        nico.stroke(150, 250, 130);
-        float size1 = b1[2];
+        for (int i = 0; i < numDots; i++) {
+            float angle = PApplet.radians(i * 360.0f / numDots); // Calculate angle for dot position
+            float x = circleRadius * PApplet.cos(angle); // Calculate x-coordinate
+            float y = circleRadius * PApplet.sin(angle); // Calculate y-coordinate
 
-        // Apply rotation transformation
-        nico.pushMatrix();
-        nico.rotateZ(rotationAngle);
-        nico.rect(-size1 / 2, -size1 / 2, size1, size1);
-        nico.popMatrix();
-
-        nico.noFill();
-        nico.camera(0, 0, 200, 0, 0, 0, 5, 0, 0);
-        nico.translate(0, 0, 0);
-        nico.strokeWeight(2);
-
-        float[] b2 = nico.getSmoothedBands();
-        for (int i = 0; i < b2.length; i++) {
-            float c = PApplet.map(i, 255, b2.length, 50, 150);
-            nico.stroke(c, 0, 102);
-            float s = b2[i];
-
-            // Apply rotation transformation
+            // Apply rotation transformation to each dot
             nico.pushMatrix();
+            nico.translate(x, y, 0);
             nico.rotateZ(rotationAngle);
-            nico.rect(-s / 2, -s / 2, s, s);
+            nico.point(0, 0); // Render dot at the transformed position
             nico.popMatrix();
         }
 
