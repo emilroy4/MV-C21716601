acd75e449d319ba5906a6a46d4f112a2f356bd5e
nicolas-ruth1 <c21388736@mytudublin.ie>
2023-04-25 18:27:49 +0100

    update6

--- MusicVisuals-master/java/src/c21716601/nico.java
+++ MusicVisuals-master/java/src/c21716601/nico.java
@@ -10,70 +10,45 @@ public class nico {
         this.nico = nico;
     }
 
-    float angle = 0;
-
-    public void setup() {
+    public void render()
+    {
         nico.noFill();
-        nico.stroke(150, 250, 130);
-    }
-
-    public void draw() {
-        nico.background(0);
-        nico.translate(nico.width / 2, nico.height / 2, 0); // move origin to center of screen
-        nico.rotateZ(angle);
-
-        render();
-
-        angle += 0.01;
-    }
-
-    public void render() {
-        nico.camera(-200, -200, 200, 0, 0, 0, 0, 0, 1);
-        nico.translate(-50, -50, 0); // move to center of screen
+        nico.camera(0, 100, 200, 0, 0, 0, 1, 0, 0);
+        nico.translate(0, 0, 0);
         nico.strokeWeight(4);
 
         float[] b = nico.getSmoothedBands();
+        nico.stroke(150, 250, 130);
         float size = b[2];
-        nico.pushMatrix();
-        nico.translate(0, 0, size / 2);
-        nico.box(size);
-        nico.popMatrix();
+        nico.circle(0, 0, size);
 
-        nico.camera(-100, 100, 200, 0, 0, 0, 0, 0, 1);
-        nico.translate(50, -50, 0); // move to center of screen
+
+        nico.noFill();
+        nico.camera(0, -100, 200, 0, 0, 0, 5, 0, 0);
+        nico.translate(0, 0, 0);
+        nico.strokeWeight(4);
+
         float[] b1 = nico.getSmoothedBands();
-        float size1 = b1[4];
-        nico.pushMatrix();
-        nico.translate(0, 0, size1 / 2);
-        nico.box(size1);
-        nico.popMatrix();
+        nico.stroke(150, 250, 130);
+        float size1 = b1[2];
+        nico.circle(0, 0, size1);
+        
 
-        nico.camera(0, 0, 200, 0, 0, 0, 0, 1, 0);
-        nico.translate(0, 50, 0); // move to center of screen
+        nico.noFill();
+        nico.camera(0, 0, 200, 0, 0, 0, 5, 0, 0);
+        nico.translate(0, 0, 0);
+        nico.strokeWeight(2);
+        
         float[] b2 = nico.getSmoothedBands();
-        float size2 = b2[6];
-        nico.pushMatrix();
-        nico.translate(0, 0, size2 / 2);
-        nico.box(size2);
-        nico.popMatrix();
-
-        nico.camera(-200, 0, 100, 0, 0, 0, 0, 1, 0);
-        nico.translate(50, 0, 0); // move to center of screen
-        float[] b3 = nico.getSmoothedBands();
-        float size3 = b3[8];
-        nico.pushMatrix();
-        nico.translate(0, 0, size3 / 2);
-        nico.box(size3);
-        nico.popMatrix();
-
-        nico.camera(200, 0, 100, 0, 0, 0, 0, 1, 0);
-        nico.translate(-50, 0, 0); // move to center of screen
-        float[] b4 = nico.getSmoothedBands();
-        float size4 = b4[10];
-        nico.pushMatrix();
-        nico.translate(0, 0, size4 / 2);
-        nico.box(size4);
-        nico.popMatrix();
+        for(int i=0; i<b2.length ; i++)
+        {
+            float c = PApplet.map(i, 255, b2.length, 50, 150);
+            nico.stroke(c, 0, 102);
+            float s = b2[i];
+            nico.pushMatrix();
+            nico.circle(0, 0, s);
+            nico.popMatrix();
+        }
     }
 
 }
