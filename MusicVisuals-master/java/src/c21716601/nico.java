6a07fbfc3a11dc3c52b40b6df9645b3f93a05d91
nicolas-ruth1 <c21388736@mytudublin.ie>
2023-04-25 17:50:34 +0100

    nicoupdate

--- MusicVisuals-master/java/src/c21716601/nico.java
+++ MusicVisuals-master/java/src/c21716601/nico.java
@@ -3,17 +3,14 @@ package c21716601;
 import processing.core.PApplet;
 
 public class nico {
-    
 
     rockstar nico;
 
-    public nico(rockstar nico)
-    {
+    public nico(rockstar nico) {
         this.nico = nico;
     }
 
-    public void render()
-    {
+    public void render() {
         nico.noFill();
         nico.camera(0, 100, 200, 0, 0, 0, 1, 0, 0);
         nico.translate(0, 0, 0);
@@ -22,8 +19,7 @@ public class nico {
         float[] b = nico.getSmoothedBands();
         nico.stroke(150, 250, 130);
         float size = b[2];
-        nico.circle(0, 0, size);
-
+        nico.rect(-size / 2, -size / 2, size, size);
 
         nico.noFill();
         nico.camera(0, -100, 200, 0, 0, 0, 5, 0, 0);
@@ -33,22 +29,20 @@ public class nico {
         float[] b1 = nico.getSmoothedBands();
         nico.stroke(150, 250, 130);
         float size1 = b1[2];
-        nico.circle(0, 0, size1);
-        
+        nico.rect(-size1 / 2, -size1 / 2, size1, size1);
 
         nico.noFill();
         nico.camera(0, 0, 200, 0, 0, 0, 5, 0, 0);
         nico.translate(0, 0, 0);
         nico.strokeWeight(2);
-        
+
         float[] b2 = nico.getSmoothedBands();
-        for(int i=0; i<b2.length ; i++)
-        {
+        for (int i = 0; i < b2.length; i++) {
             float c = PApplet.map(i, 255, b2.length, 50, 150);
             nico.stroke(c, 0, 102);
             float s = b2[i];
             nico.pushMatrix();
-            nico.circle(0, 0, s);
+            nico.rect(-s / 2, -s / 2, s, s);
             nico.popMatrix();
         }
     }
