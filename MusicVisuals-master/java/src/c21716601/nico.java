233a34834f60e432f394ccb27bd20559f9bd2303
nicolas-ruth1 <c21388736@mytudublin.ie>
2023-04-25 18:53:15 +0100

    update10

--- MusicVisuals-master/java/src/c21716601/nico.java
+++ MusicVisuals-master/java/src/c21716601/nico.java
@@ -11,8 +11,6 @@ public class nico {
     }
 
     public float rotationAngle = 0.0f;
-    public float circleRadius = 100.0f;
-    public int numDots = 12;
 
     public void render() {
         nico.noFill();
@@ -24,26 +22,48 @@ public class nico {
         nico.stroke(0, 255, 0); // Green stroke color
         float size = b[2];
 
-        // Apply rotation transformation to the squares
+        // Apply rotation transformation
         nico.pushMatrix();
         nico.rotateZ(rotationAngle);
         nico.rect(-size / 2, -size / 2, size, size);
         nico.popMatrix();
 
-        // Render spinning dots around the squares
         nico.noFill();
-        nico.stroke(255); // White stroke color for dots
+        nico.camera(0, -100, 200, 0, 0, 0, 5, 0, 0);
+        nico.translate(0, 0, 0);
+        nico.strokeWeight(4);
 
-        for (int i = 0; i < numDots; i++) {
-            float angle = PApplet.radians(i * 360.0f / numDots); // Calculate angle for dot position
-            float x = circleRadius * PApplet.cos(angle); // Calculate x-coordinate
-            float y = circleRadius * PApplet.sin(angle); // Calculate y-coordinate
+        float[] b1 = nico.getSmoothedBands();
+        nico.stroke(0, 255, 0); // Green stroke color
+        float size1 = b1[2];
 
-            // Apply rotation transformation to each dot
+        // Apply rotation transformation
+        nico.pushMatrix();
+        nico.rotateZ(rotationAngle);
+        nico.rect(-size1 / 2, -size1 / 2, size1, size1);
+        nico.popMatrix();
+
+        nico.noFill();
+        nico.camera(0, 0, 200, 0, 0, 0, 5, 0, 0);
+        nico.translate(0, 0, 0);
+        nico.strokeWeight(2);
+
+        float[] b2 = nico.getSmoothedBands();
+        for (int i = 0; i < b2.length; i++) {
+            float c = nico.map(i, 0, b2.length, 0, 360); // Map the index to an angle
+            float r = size + (b2[i] * 0.5f); // Calculate the radius of the circle
+
+            // Calculate the position of the dot using polar coordinates
+            float x = r * nico.cos(nico.radians(c));
+            float y = r * nico.sin(nico.radians(c));
+
+            nico.stroke(0, 255, 0); // Green stroke color
+
+            // Apply rotation transformation
             nico.pushMatrix();
-            nico.translate(x, y, 0);
             nico.rotateZ(rotationAngle);
-            nico.point(0, 0); // Render dot at the transformed position
+            nico.translate(x, y);
+            nico.ellipse(0, 0, 10, 10); // Draw the dot
             nico.popMatrix();
         }
 
