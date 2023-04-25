3d58fde646ffc8e57eab407e97ff7cff5cf7fd62
emilroy4 <emilroy100@gmail.com>
2023-04-25 20:25:54 +0100

    stars

--- MusicVisuals-master/java/src/c21716601/emil.java
+++ MusicVisuals-master/java/src/c21716601/emil.java
@@ -4,29 +4,40 @@ import processing.core.PApplet;
 //import ie.tudublin.Visual;
 
 public class emil {
-    
+
     rockstar emil;
 
     float smallCircleSize = 0; // initial size of small circle
     float smallCircleAngle = 0; // initial angle of rotation for small circle
 
-    public emil(rockstar emil)
-    {
+    public emil(rockstar emil) {
         this.emil = emil;
     }
 
-    public void render()
-    {
+    public void render() {
         emil.noFill();
         emil.camera(0, 100, 200, 0, 0, 0, 1, 0, 0);
         emil.translate(0, 0, 0);
         emil.strokeWeight(4);
+        float[] stars = new float[500];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = emil.random(-emil.width, emil.width);
+        }
+
+        emil.stroke(250);
+        for (int i = 0; i < stars.length; i++) {
+            float speed = emil.map(emil.getSmoothedAmplitude(), 0, 1, 1, 5);
+            stars[i] += speed;
+            if (stars[i] > emil.width) {
+                stars[i] = emil.random(-emil.width, 0);
+            }
+            emil.point(stars[i], emil.random(-emil.height, emil.height));
+        }
 
         float[] b = emil.getSmoothedBands();
         emil.stroke(250);
         float size = b[2];
         emil.circle(0, 0, size);
-
 
         emil.noFill();
         emil.camera(0, -100, 200, 0, 0, 0, 5, 0, 0);
@@ -37,21 +48,19 @@ public class emil {
         emil.stroke(250);
         float size1 = b1[2];
         emil.circle(0, 0, size1);
-        
 
         emil.noFill();
         emil.camera(0, 0, 200, 0, 0, 0, 5, 0, 0);
         emil.translate(0, 0, 0);
         emil.strokeWeight(2);
-        
+
         float[] b2 = emil.getSmoothedBands();
-        for(int i=0; i<b2.length ; i++)
-        {
+        for (int i = 0; i < b2.length; i++) {
             float c = PApplet.map(i, 255, b2.length, 50, 150);
-            emil.stroke(0,100,0);
+            emil.stroke(0, 100, 0);
             float s = b2[i];
             emil.pushMatrix();
-            emil.translate(0, 0, s/2); // move to center of the small circle
+            emil.translate(0, 0, s / 2); // move to center of the small circle
             emil.rotateZ(PApplet.radians(smallCircleAngle)); // rotate around the Z-axis
             emil.circle(0, 0, smallCircleSize); // draw the small circle
             emil.popMatrix();
@@ -59,6 +68,6 @@ public class emil {
 
         // animate and rotate the small circle
         smallCircleSize = PApplet.map(PApplet.sin(PApplet.radians(smallCircleAngle)), -1, 1, 5, 10);
-        smallCircleAngle += 5;
+        smallCircleAngle +=-5;
     }
 }
